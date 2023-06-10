a37f3579a8a614f17b59644eca180af8c3a54622
PeterTheOne <petertheone@gmail.com>
2023-06-10 18:55:03 +0200

    named import fixes warning about provider.
    still not sure about linking the contract.

--- src/tokens-plugin.ts
+++ src/tokens-plugin.ts
@@ -1,4 +1,4 @@
-import Web3PluginBase, { Contract } from 'web3';
+import { Web3PluginBase, Contract } from 'web3';
 
 import ERC20 from './artifacts/ERC20';
 import ERC721 from './artifacts/ERC721';
@@ -10,23 +10,33 @@ export class TokensPlugin extends Web3PluginBase {
   public pluginNamespace = 'tokens';
 
   public erc20(address: string): Contract<typeof ERC20.abi> {
-    return new Contract(ERC20.abi, address);
+    const erc20Contract = new Contract(ERC20.abi, address);
+    erc20Contract.link(this);
+    return erc20Contract;
   }
 
   public erc721(address: string): Contract<typeof ERC721.abi> {
-    return new Contract(ERC721.abi, address);
+    const erc721Contract = new Contract(ERC721.abi, address);
+    erc721Contract.link(this);
+    return erc721Contract;
   }
 
   public erc777(address: string): Contract<typeof ERC777.abi> {
-    return new Contract(ERC777.abi, address);
+    const erc777Contract = new Contract(ERC777.abi, address);
+    erc777Contract.link(this);
+    return erc777Contract;
   }
 
   public erc1155(address: string): Contract<typeof ERC1155.abi> {
-    return new Contract(ERC1155.abi, address);
+    const erc1155Contract = new Contract(ERC1155.abi, address);
+    erc1155Contract.link(this);
+    return erc1155Contract;
   }
 
   public erc4626(address: string): Contract<typeof ERC4626.abi> {
-    return new Contract(ERC4626.abi, address);
+    const erc4626Contract = new Contract(ERC4626.abi, address);
+    erc4626Contract.link(this);
+    return erc4626Contract;
   }
 }
 
